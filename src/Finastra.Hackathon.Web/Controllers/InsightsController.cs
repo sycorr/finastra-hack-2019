c2d9e4f6576348f95e18b15af3426c433bac81c9
Max Pool <mpool@sycorr.com>
2019-12-04 12:04:03 -0600

    Fill in customer side insights page.

--- src/Finastra.Hackathon.Web/Controllers/InsightsController.cs
+++ src/Finastra.Hackathon.Web/Controllers/InsightsController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Finastra.Hackathon.ML;
+using Finastra.Hackathon.Web.Models.Insights;
+using Microsoft.AspNetCore.Mvc;
 namespace Finastra.Hackathon.Web.Controllers
 {
     public class InsightsController : Controller
     {
         public ActionResult Index()
         {
-            return View("~/Views/Insights/Index.cshtml");
+            var predictor = new TimeSeriesPredictor();
+
+            var data = StaticData.AccountingPrinciples.ToList();
+
+            if(SimulationConfiguration.CustomerAlteredRationAnalysis)
+                data.Add(SimulationConfiguration.RatioAnalysis);
+
+            var model = new IndexModel();
+            model.RatioAnalyses = predictor.Predict(data).OrderBy(x => x.Date);
+            model.NaicInformation = StaticData.NAIC.Single(x => x.Id == SimulationConfiguration.SelectedNAIC);
+            model.RmaInformations = StaticData.RMA.Where(x => x.NAICId == SimulationConfiguration.SelectedNAIC).OrderBy(x => x.Date);
+
+            return View(model);
         }
     }
 }
---
+++ src/Finastra.Hackathon.Web/Models/Insights/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Finastra.Hackathon.Accounting;
+using Finastra.Hackathon.NAICS;
+
+namespace Finastra.Hackathon.Web.Models.Insights
+{
+    public class IndexModel
+    {
+        public IEnumerable<RatioAnalysis> RatioAnalyses { get; set; }
+        public IEnumerable<RMAInformation> RmaInformations { get; set; }
+        public NAICInformation NaicInformation { get; set; }
+
+    }
+}
