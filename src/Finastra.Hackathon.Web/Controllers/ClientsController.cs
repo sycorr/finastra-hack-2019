b1ac134f21bbf8fc24a25214cd40ad17be167c7a
bswang <bswang@sycorr.com>
2019-12-04 19:31:34 -0600

    Client Details / Insights

--- src/Finastra.Hackathon.Web/Controllers/ClientsController.cs
+++ src/Finastra.Hackathon.Web/Controllers/ClientsController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using Finastra.Hackathon.ML;
+using Finastra.Hackathon.Web.Models.Insights;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 
 namespace Finastra.Hackathon.Web.Controllers
 {
@@ -17,7 +21,19 @@ namespace Finastra.Hackathon.Web.Controllers
 
         public ActionResult Insights()
         {
-            return View();
+            var predictor = new TimeSeriesPredictor();
+
+            var data = StaticData.AccountingPrinciples.ToList();
+
+            if (SimulationConfiguration.CustomerAlteredRationAnalysis)
+                data.Add(SimulationConfiguration.RatioAnalysis);
+
+            var model = new IndexModel();
+            model.RatioAnalyses = predictor.Predict(data).OrderBy(x => x.Date);
+            model.NaicInformation = StaticData.NAIC.Single(x => x.Id == SimulationConfiguration.SelectedNAIC);
+            model.RmaInformations = StaticData.RMA.Where(x => x.NAICId == SimulationConfiguration.SelectedNAIC).OrderBy(x => x.Date);
+
+            return View(model);
         }
     }
 }
